a907c74223f2870db0e7e15629c1b55fc85f50f5
Andrew <andrew.kennedy27@outlook.com>
2019-07-30 21:02:23 +0100

    article data

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Andrew Kennedy',
+            'create_date':'30-07-2019'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Jane Doe',
+            'create_date':'30-07-2019'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Andrew Kennedy',
+            'create_date':'30-07-2019'
+        }
+    ]
+    return articles
