618afb30e3d67e05f854849964e4c7c9697a5b08
Andrew <andrew.kennedy27@outlook.com>
2019-07-31 22:15:25 +0100

    register route, register function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, flask, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
-from flask_mysqldb import flask_mysqldb
+from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
@@ -33,6 +33,12 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
